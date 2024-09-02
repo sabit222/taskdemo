99e7ebb1e9190cc92c350a665155a872d0d8eacb
sabit222 <sr.rakhim@gmail.com>
2024-09-02 17:52:59 +0500

    Delete taskdemo directory

--- taskdemo/src/main/java/com/example/taskdemo/Priority.java
+++
@@ -1,7 +0,0 @@
-package com.example.taskdemo;
-
-public enum Priority {
-    LOW,
-    MEDIUM,
-    HIGH
-}
--- taskdemo/src/main/java/com/example/taskdemo/repository/TaskRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.taskdemo.repository;
-
-import com.example.taskdemo.model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-
-}
